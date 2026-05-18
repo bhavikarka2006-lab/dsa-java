a2e4780b78544e67a488b0cffbc81300494703b3
bhavesh reddy <bhavikarka2006@gmail.com>
2026-05-18 15:52:37 +0530

    Update Hello.java

---
+++ JAVA/Hello.java
@@ -0,0 +1,8 @@
+package JAVA;
+public class Hello {
+     public static void main(String[] args) {
+        System.out.println("Hello Bhavesh");
+        System.out.println("Hello");
+    }
+    
+}
